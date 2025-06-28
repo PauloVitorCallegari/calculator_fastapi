fd6c027763962dff2ae23ed7811860f6b6aa1f70
Paulo Vitor <pvitorcalegaridalvi@gmail.com>
2025-06-28 00:50:09 -0300

    fix: refazendo publish.yaml e setup.py

---
+++ calculadora-fastapi/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calculadora-fastapi-ps",
+    version="0.1.1",
+    description="Uma biblioteca simples de calculadora",
+    author="Paulo Vitor",
+    author_email="kngod4@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
